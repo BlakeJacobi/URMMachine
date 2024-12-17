40dfacee1899e62795d2832994b9919200cbe922
BlakeJacobi <blake.jacobi@windowslive.com>
2024-12-18 08:26:08 +1100

    My URMMachine

--- Main.java
+++ Main.java
@@ -25,17 +25,5 @@ public class Main {
 
         urMachine.run();
 
-        //urMachine2 is a test case of a program that doesn't terminate and loops infinitely (URM simulation 3.b)
-        String input2 = "config[9,7]\njump(1,2,6)\nsucc(2)\nsucc(3)\nzero(8)\njump(1,2,6)\ntransfer(3,1)\nZERO(2)\nZero(3)\ntransfer(1,9)\njump(1,1,1)"; //final instruction will cause this program to loop infinitely
-        URMLexer lexer2 = new URMLexer(CharStreams.fromString(input2));
-        URMParser parser2 = new URMParser(new CommonTokenStream(lexer2));
-        ParseTree tree2 = parser2.program();
-
-        MyURMVisitor visitor2 = new MyURMVisitor();
-        visitor2.visit(tree2);
-        URMachine urMachine2 = visitor2.getURMachine();
-        //comment out below line to see just 3.a without the infinite looping of 3.b.
-        //urMachine2.run();
-
     }
 }
